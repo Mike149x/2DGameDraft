59b27b86a51571ea685268e47b53e09af406003b
Mike149x <mikeismike148@gmail.com>
2022-03-08 13:23:28 +1300

    Established Variables

--- 2DGameDraft/Scenes/PlayerCharacter.gd
+++ 2DGameDraft/Scenes/PlayerCharacter.gd
@@ -1,9 +1,15 @@
 extends KinematicBody2D
 
+export (int) var speed = 120 
+export (int) var jump_speed = -180
+export (int) var gravity = 400
+export (int) var slide_speed = 400
 
+var velocity = Vector2.ZERO
 
+export (float) var friction = 10
+export (float) var acceleration = 25
 
+enum state {IDLE, RUNNING, PUSHING, ROLLING, JUMP, FALL, ATTACK}
 
-
-
-
+onready var player_state = state.IDLE
