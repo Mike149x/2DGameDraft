59fb899d0363d4d71679f122b89c2be757e97de6
Mike149x <mikeismike148@gmail.com>
2022-06-27 12:25:29 +1200

    Begun BugFixing

--- 2DGameDraft/Scenes/PlayerCharacter.gd
+++ 2DGameDraft/Scenes/PlayerCharacter.gd
@@ -83,3 +83,9 @@ func _physics_process(delta):        #checks every frame
 
 
 
+
+
+func _on_DeathZone_area_entered(area):
+	if area.is_in_group("Deadly"):
+		if GameStats.check_reset() == false:
+			global_position = GameStats.get_spawn().global_position
