cab8affdbc4b084561a5161120e46a113fe0d3b8
Mike149x <mikeismike148@gmail.com>
2022-06-28 13:23:02 +1200

    Redoing Animations, Deadly Areas

--- 2DGameDraft/Scenes/SpikeTrap.gd
+++ 2DGameDraft/Scenes/SpikeTrap.gd
@@ -6,12 +6,13 @@ export (int) var wait_time = 0.6
 
 func _ready():
 	
-	$AnimationPlayer.play("SpikeTrigger")
+#	$AnimationPlayer.play("SpikeTrigger")
+	pass
 
 
-func _on_Area2D_body_entered(body):
-	if body.is_in_group("Player"):
-		print("player dead")
-		if GameStats.check_reset() == false:
-			body.global_position = GameStats.get_spawn().global_position
+#func _on_Area2D_body_entered(body):
+#	if body.is_in_group("Player"):
+#		print("player dead")
+#		if GameStats.check_reset() == false:
+#			body.global_position = GameStats.get_spawn().global_position
 
