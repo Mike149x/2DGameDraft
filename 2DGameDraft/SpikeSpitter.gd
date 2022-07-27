67da318a856db9bc5edc77edad007aec92a1ea19
Mike149x <mikeismike148@gmail.com>
2022-07-27 12:24:31 +1200

    SpikeSpitter - now onto moving flames

--- 2DGameDraft/SpikeSpitter.gd
+++ 2DGameDraft/SpikeSpitter.gd
@@ -1,19 +1,13 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var arrow = $Arrow
+var arrow_speed = 200
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _process(delta):
+	arrow.translate(Vector2.LEFT * arrow_speed * delta)
+	
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 
 func _on_SpikeEnd_area_entered(area):
