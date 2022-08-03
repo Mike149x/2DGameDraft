a9ecaaea9b490f80e202ddfbf7435820974e7045
Mike149x <mikeismike148@gmail.com>
2022-08-03 20:04:40 +1200

    Single Flame and Spinning Flames

---
+++ 2DGameDraft/Scenes/SpinningFlame.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+
+func _ready():
+	pass
+
+
+func _process(delta):
+	$RotationPoint.rotation_degrees += 1
+	$Flame.global_position = $RotationPoint/FlamePos.global_position
+	$Flame2.global_position = $RotationPoint/FlamePos2.global_position
+	$Flame3.global_position = $RotationPoint/FlamePos3.global_position
+	$Flame4.global_position = $RotationPoint/FlamePos4.global_position
+	$Flame5.global_position = $RotationPoint/FlamePos5.global_position
+	$Flame6.global_position = $RotationPoint/FlamePos6.global_position
+	$Flame7.global_position = $RotationPoint/FlamePos7.global_position
+	$Flame8.global_position = $RotationPoint/FlamePos8.global_position
+
+
+
+
+
+
+
+
+
+
