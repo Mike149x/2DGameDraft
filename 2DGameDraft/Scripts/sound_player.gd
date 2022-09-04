032fb86e15392aa80b331df1a667f659839c552e
Mike149x <mikeismike148@gmail.com>
2022-09-05 09:43:16 +1200

    Began SoundPlayer Work

---
+++ 2DGameDraft/Scripts/sound_player.gd
@@ -0,0 +1,22 @@
+extends Node
+
+
+
+var music_tracks = {
+	"field 1":"res://Resources/Free RPG Music Pack - SVL/field_theme_1.wav",
+	"field 2":"res://Resources/Free RPG Music Pack - SVL/field_theme_2.wav",
+}
+
+var sound_effects = {
+	"jump 1":"res://Resources/MiniFantasy - Dungeon SFX Pack/12_human_jump_1.wav",
+	"jump 2":"res://Resources/MiniFantasy - Dungeon SFX Pack/12_human_jump_2.wav",
+	"jump 3":"res://Resources/MiniFantasy - Dungeon SFX Pack/12_human_jump_3.wav",
+}
+	
+
+
+
+
+func _ready():
+	pass # Replace with function body.
+
