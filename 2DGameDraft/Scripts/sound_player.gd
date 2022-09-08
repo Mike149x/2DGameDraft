1d6ffcc11ba38d06cc2d69fd6e186a20d8e42f48
Mike149x <mikeismike148@gmail.com>
2022-09-08 13:24:28 +1200

    sound player minor commit

--- 2DGameDraft/Scripts/sound_player.gd
+++ 2DGameDraft/Scripts/sound_player.gd
@@ -1,22 +1,27 @@
 extends Node
 
-
+onready var music = AudioStreamPlayer.new()
 
 var music_tracks = {
 	"field 1":"res://Resources/Free RPG Music Pack - SVL/field_theme_1.wav",
-	"field 2":"res://Resources/Free RPG Music Pack - SVL/field_theme_2.wav",
 }
 
 var sound_effects = {
 	"jump 1":"res://Resources/MiniFantasy - Dungeon SFX Pack/12_human_jump_1.wav",
-	"jump 2":"res://Resources/MiniFantasy - Dungeon SFX Pack/12_human_jump_2.wav",
-	"jump 3":"res://Resources/MiniFantasy - Dungeon SFX Pack/12_human_jump_3.wav",
 }
 	
 
+var music_db = 1
+var sound_db = 1
 
+func change_music_db(val):
+	music_db = linear2db(val)
+
+func change_sound_db(val):
+	sound_db = linear2db(val)
 
 
 func _ready():
-	pass # Replace with function body.
-
+	music.stream = load(music_tracks["field 1"])
+	add_child(music)
+	music.play()
