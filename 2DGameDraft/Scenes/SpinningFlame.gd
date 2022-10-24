0ef9ce07258cf6ee81500a73fce496b541b6ce4a
Mike149x <mikeismike148@gmail.com>
2022-10-24 18:10:46 +1300

    WHOLE GAME LAYOUT DONE

--- 2DGameDraft/Scenes/SpinningFlame.gd
+++ 2DGameDraft/Scenes/SpinningFlame.gd
@@ -1,16 +1,18 @@
 extends Node2D
 
-export (int) var rotation_direction = 1
+export (int) var rotation_direction = 1 #How many degrees the flamewheel will rotate by
+									   #By making it an export variable, we can change signs to change direction
+									   #of rotation for specific flamewheels
 func _ready():
 	pass
 
 
 func _process(delta):
-	$RotationPoint.rotation_degrees += rotation_direction
-	$Flame.global_position = $RotationPoint/FlamePos.global_position
-	$Flame2.global_position = $RotationPoint/FlamePos2.global_position
-	$Flame3.global_position = $RotationPoint/FlamePos3.global_position
-	$Flame4.global_position = $RotationPoint/FlamePos4.global_position
+	$RotationPoint.rotation_degrees += rotation_direction       #we set the center of the flamewheel as RotationPoint
+	$Flame.global_position = $RotationPoint/FlamePos.global_position   #then add positions as its child.
+	$Flame2.global_position = $RotationPoint/FlamePos2.global_position   #Now if we rotate the center, the children also rotate
+	$Flame3.global_position = $RotationPoint/FlamePos3.global_position #so the last step is to make the flames at the position 
+	$Flame4.global_position = $RotationPoint/FlamePos4.global_position # of the children
 	$Flame5.global_position = $RotationPoint/FlamePos5.global_position
 	$Flame6.global_position = $RotationPoint/FlamePos6.global_position
 	$Flame7.global_position = $RotationPoint/FlamePos7.global_position
--- 2DGameDraft/Scenes/TitleScreen.gd
+++ 2DGameDraft/Scenes/TitleScreen.gd
@@ -2,15 +2,15 @@ extends Control
 
 
 func _ready():
-	$AnimationPlayer.play("EmergeFromFog")
-	$MenuButtons.hide()
+	$AnimationPlayer.play("EmergeFromFog") #On Start up, play this short intro,
+	$MenuButtons.hide()                    #and keep menu buttons hidden for duration of intro
 
 
-func _on_AnimationPlayer_animation_finished(anim_name):
+func _on_AnimationPlayer_animation_finished(anim_name):  #when intro is done:
 	if anim_name == "EmergeFromFog":
 		#$ParallaxBackground/TitleLayer/Title.set_modulate(1,0,0,0)
-		$MenuButtons.visible = true
-		pass
+		$MenuButtons.visible = true   #make the buttons appear, to make it look like either the titlescreen loads,
+		pass                          #or that the forest has emerged from the fog.
 		
 
 
